e42cba86ebe02085da59bcbc223dc648b4cee0f0
Baki OZTURK <22.exe.94@gmail.com>
2021-06-30 19:38:43 +0300

    Adding Items

--- GuardApp/Form1.Designer.cs
+++ GuardApp/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace GuardApp
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,21 +29,82 @@ namespace GuardApp
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnRank = new System.Windows.Forms.Button();
+            this.btnPersonal = new System.Windows.Forms.Button();
+            this.btnGuard = new System.Windows.Forms.Button();
+            this.btnMonthly = new System.Windows.Forms.Button();
+            this.btnLastGuard = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnRank
+            // 
+            this.btnRank.Location = new System.Drawing.Point(252, 137);
+            this.btnRank.Name = "btnRank";
+            this.btnRank.Size = new System.Drawing.Size(145, 42);
+            this.btnRank.TabIndex = 0;
+            this.btnRank.Text = "Rütbe Ekle";
+            this.btnRank.UseVisualStyleBackColor = true;
+            // 
+            // btnPersonal
+            // 
+            this.btnPersonal.Location = new System.Drawing.Point(63, 137);
+            this.btnPersonal.Name = "btnPersonal";
+            this.btnPersonal.Size = new System.Drawing.Size(145, 42);
+            this.btnPersonal.TabIndex = 1;
+            this.btnPersonal.Text = "Personel Ekle";
+            this.btnPersonal.UseVisualStyleBackColor = true;
+            // 
+            // btnGuard
+            // 
+            this.btnGuard.Location = new System.Drawing.Point(449, 137);
+            this.btnGuard.Name = "btnGuard";
+            this.btnGuard.Size = new System.Drawing.Size(145, 42);
+            this.btnGuard.TabIndex = 2;
+            this.btnGuard.Text = "Nöbet Ekle";
+            this.btnGuard.UseVisualStyleBackColor = true;
+            // 
+            // btnMonthly
+            // 
+            this.btnMonthly.Location = new System.Drawing.Point(63, 50);
+            this.btnMonthly.Name = "btnMonthly";
+            this.btnMonthly.Size = new System.Drawing.Size(531, 42);
+            this.btnMonthly.TabIndex = 3;
+            this.btnMonthly.Text = "Aylık Nöbet Oluştur";
+            this.btnMonthly.UseVisualStyleBackColor = true;
+            // 
+            // btnLastGuard
+            // 
+            this.btnLastGuard.Location = new System.Drawing.Point(63, 201);
+            this.btnLastGuard.Name = "btnLastGuard";
+            this.btnLastGuard.Size = new System.Drawing.Size(531, 30);
+            this.btnLastGuard.TabIndex = 4;
+            this.btnLastGuard.Text = "Geçmiş Nöbetleri Gör";
+            this.btnLastGuard.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(659, 330);
+            this.Controls.Add(this.btnLastGuard);
+            this.Controls.Add(this.btnMonthly);
+            this.Controls.Add(this.btnGuard);
+            this.Controls.Add(this.btnPersonal);
+            this.Controls.Add(this.btnRank);
+            this.Name = "FormMain";
+            this.Text = "[NTP] Nöbet Takip Programı";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRank;
+        private System.Windows.Forms.Button btnPersonal;
+        private System.Windows.Forms.Button btnGuard;
+        private System.Windows.Forms.Button btnMonthly;
+        private System.Windows.Forms.Button btnLastGuard;
     }
 }
 
