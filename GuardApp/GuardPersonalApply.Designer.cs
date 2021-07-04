f6e890012768f257abb98aaf41943ee1bd417b6b
Baki OZTURK <22.exe.94@gmail.com>
2021-07-04 21:46:28 +0300

    Click events

---
+++ GuardApp/GuardPersonalApply.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace GuardApp
+{
+    partial class GuardPersonalApply
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(26, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 259);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(161, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(282, 134);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(111, 68);
+            this.btnApply.TabIndex = 2;
+            this.btnApply.Text = "Kişiyi Tarihe Ata";
+            this.btnApply.UseVisualStyleBackColor = true;
+            // 
+            // GuardPersonalApply
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 361);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GuardPersonalApply";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GuardPersonalApply";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+       
+        private System.Windows.Forms.Button btnApply;
+    }
+}
--- GuardApp/GuardProgress.cs
+++ GuardApp/GuardProgress.cs
@@ -18,20 +18,20 @@ namespace GuardApp
             InitializeComponent();
         }
         private List<FlowLayoutPanel> flowLayoutPanels = new List<FlowLayoutPanel>();
-        private CultureInfo uiCulture1 = new CultureInfo("tr-TR");
+        private CultureInfo trCulture = new CultureInfo("tr-TR");
         //Program açıldığında bir sonraki ay default olarak gelsin.
         //nextDate üzerinden yakalacagız -1 Month +1Month next prevleri
         private DateTime nextDate = DateTime.Today.AddMonths(1);
         private void GuardProgress_Load(object sender, EventArgs e)
         {
-            lnkToday.Text = DateTime.Today.ToString("dd/MMMM/yyyy", uiCulture1);
+            lnkToday.Text = DateTime.Today.ToString("dd/MMMM/yyyy", trCulture);
             GenerateDayPanel(42);
             DisplayCurrentDate();
         }
 
         private void DisplayCurrentDate()
         {
-            lblMonthAndYear.Text = nextDate.ToString("MMMM, yyyy", uiCulture1);
+            lblMonthAndYear.Text = nextDate.ToString("MMMM, yyyy", trCulture);
             AddLabelDayToFlDay(GetFirstDayOfWeekOfCurrentDate(), GetTotalDaysOfDate());
         }
         private void PrevMonth()
@@ -59,16 +59,38 @@ namespace GuardApp
                 flowLayoutPanel.BackColor = Color.White;
                 flowLayoutPanel.Margin = new Padding(2, 2, 2, 2);
                 flowLayoutPanel.BorderStyle = BorderStyle.FixedSingle;
+                flowLayoutPanel.Click += FlowLayoutPanel_Click;
                 flDays.Controls.Add(flowLayoutPanel);
                 flowLayoutPanels.Add(flowLayoutPanel);
             }
+        }
+
+        private void FlowLayoutPanel_Click(object sender, EventArgs e)
+        {
+
+            FlowLayoutPanel clickedFl = (FlowLayoutPanel)sender;
+
+            int clickedFlowLayoutNumber = Convert.ToInt32(clickedFl.Name.Remove(0, 5));
+
+            //Sadece numaralı olan flowlayoutlar tıklandıgında tıklanılsın
+            if (GetFirstDayOfWeekOfCurrentDate() - 1 < clickedFlowLayoutNumber && clickedFlowLayoutNumber < GetFirstDayOfWeekOfCurrentDate() + GetTotalDaysOfDate())
+            {       
+                int clickedDay = clickedFlowLayoutNumber - GetFirstDayOfWeekOfCurrentDate() + 1;
+                var clickedDate = new DateTime(nextDate.Year, nextDate.Month, clickedDay).ToString("dd MMMM yyyy", trCulture);
+               
+                MessageBox.Show(clickedDate);
+                //Buraya GuardPersonalApply formu acıp ılgılı tarıhe seçilen personeli atayacagız.
+            }
+            else
+                MessageBox.Show("NO");
+            
         }
 
         //Kaçıncı flatlayouttan başlasın ve ayda kaç gun olacak. (Günleri oluşturma)
         private void AddLabelDayToFlDay(int startDayAtFlNumber, int totalDaysInMonth)
         {
             flowLayoutPanels.ForEach(x => x.Controls.Clear());
-              
+
             for (int i = 1; i < totalDaysInMonth + 1; i++)
             {
                 Label lbl = new Label();
@@ -95,7 +117,7 @@ namespace GuardApp
 
         private void lnkToday_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            lblMonthAndYear.Text = DateTime.Today.ToString("MMMM, yyyy", uiCulture1);
+            lblMonthAndYear.Text = DateTime.Today.ToString("MMMM, yyyy", trCulture);
             nextDate = DateTime.Today;
             AddLabelDayToFlDay(GetFirstDayOfWeekOfCurrentDate(), GetTotalDaysOfDate());
         }
